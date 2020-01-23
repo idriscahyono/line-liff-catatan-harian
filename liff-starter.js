990edacd5df7c1b3370c3fbad38bb6f6d5233f2e
Idris Cahyono <37612490+idriscahyono@users.noreply.github.com>
2020-01-23 19:22:10 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     const useNodeJS = false; // if you are not using a node server, set this value to false
-    const defaultLiffId = "line://app/1653794782-3MQgQOZq"; // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1653794782-3MQgQOZq"; // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
