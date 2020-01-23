a5c25f51e3e660d84fbfd65290cd7abe912a5839
idriscahyono <idriscahyono@gmail.com>
2020-01-23 17:44:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get("/", function (req, res) {
+    res.send("Hello World");
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server Running")
+})
